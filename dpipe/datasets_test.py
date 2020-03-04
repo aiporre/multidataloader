ff80b7ddcdbe7c9df88c71e012b0a14bd6396099
arielin <ariel.iporre.rivas@gmail.com>
2020-03-04 16:33:22 +0100

    added todo

--- dpipe/datasets_test.py
+++ dpipe/datasets_test.py
@@ -9,5 +9,6 @@ class TestFromListFactory(unittest.TestCase):
     def test_videos_label(self):
         dataset = make_dataset('video', 'label', x_path=DATAPATH_VIDEOS, x_size=(128,128)).build()
         self.assertEqual(len(list(dataset.as_numpy_iterator())), 4)
+    # TODO: implement test cases video video, video image, image video, label video and label image
 if __name__ == '__main__':
     unittest.main()
