917fc423276888248fff5a4fa8f3ed043b87335b
Ariel <ariel.iporre.rivas@gmail.com>
2020-12-14 06:26:34 +0100

    upgrade tf requirement to tf2.4.0

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from pkg_resources import parse_requirements
 parsed_requirements = parse_requirements('requirements.txt')
 requirements = [str(r) for r in parsed_requirements]
 setup(name='dapipe',
-      version='0.2',
+      version='0.3',
       description='Creates dataset builder objects',
       url='https://github.com/aiporre/multidataloader.git',
-      download_url='https://github.com/aiporre/multidataloader/archive/v0.2.tar.gz',
+      download_url='https://github.com/aiporre/multidataloader/archive/v0.3.tar.gz',
       author='Ariel Iporre',
       author_email='ariel.iporre.rivas@gmail.com',
       long_description=open('README.md').read(),
